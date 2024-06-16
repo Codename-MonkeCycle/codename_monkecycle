fe66836509f16c2815f8ffe7c469091c9536a102
Anssi Jämsén <61498900+ZiZZnA@users.noreply.github.com>
2024-06-17 02:19:56 +0300

    Menu buttons, quit functionality

---
+++ Assets/UI/Scripts/QuitGame.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    public static void QuitApp()
+    {
+        Application.Quit();
+    }
+
+}
